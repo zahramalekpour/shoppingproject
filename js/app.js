c5e39802ab769e442d8b74abc139f77de3968fbb
zahramalekpour <malekpourzahra5@gmail.com>
2025-09-11 09:02:58 +0330

    push

---
+++ js/app.js
@@ -0,0 +1,18 @@
+import Cart from "../models/Cart.js";
+import Products from "../models/products.js";
+import { fetchData } from "../utiles/httpRequest.js";
+
+const productsNode = document.getElementById("products");
+const cartListNode = document.getElementById("cart-list");
+const totalPriceNode = document
+  .getElementById("total-price")
+  .querySelector("span");
+
+async function render() {
+  const productsData = await fetchData();
+  const cartInstance = new Cart(cartListNode, totalPriceNode);
+  const productsInstance = new Products(productsNode, productsData, cartInstance);
+  productsInstance.showProducts();
+};
+
+document.addEventListener("DOMContentLoaded", render);
